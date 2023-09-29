377a7f0cd03d92c62372ee6fba08b5655c2403e1
seniorQAautomationEngineer <oleksii.lavrenin@gmail.com>
2023-09-29 16:37:48 -0400

    Created project. Completed custom FeedPost component.

---
+++ socialNet/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import FeedPost from './src/components/FeedPost';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <FeedPost />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
