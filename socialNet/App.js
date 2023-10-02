d9c759b1d8c1c05773be3777f02809c8521cb364
seniorQAautomationEngineer <oleksii.lavrenin@gmail.com>
2023-10-02 14:12:22 -0400

    Completed integration with Amplify.

--- socialNet/App.js
+++ socialNet/App.js
@@ -2,6 +2,10 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import Navigator from "./src/navigation";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Amplify } from "aws-amplify";
+import awsconfig from "./src/aws-exports";
+
+Amplify.configure(awsconfig);
 
 export default function App() {
   return (
