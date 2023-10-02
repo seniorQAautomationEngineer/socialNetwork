46ae32af6434291f014805662e57934eb94ed7e7
seniorQAautomationEngineer <oleksii.lavrenin@gmail.com>
2023-10-02 17:17:40 -0400

    Created User and Post Data models.

--- socialNet/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ socialNet/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "socialNet": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "socialNet": {
       "AppClientID": "string",
